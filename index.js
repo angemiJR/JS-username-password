815acd1ff20913f78e9313c4288afe452c4fd819
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-10-25 13:31:08 +0200

    second condition done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function ValidPassword(password, username){
-if (password.length <8){
+if (password.length <8 || password.includes(" ")){
     return false;
 } else {
     return true;
 }
 }
 
-console.log(ValidPassword("hajklmnj", "tri"));
+console.log(ValidPassword("hajkl mnj", "tri"));
